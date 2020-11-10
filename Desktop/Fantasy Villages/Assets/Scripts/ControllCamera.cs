ee4ff7506564581690053623da5c1c279c198297
cniki006 <cniki006@edu.xamk.fi>
2020-11-10 10:44:41 +0200

    Initial Commit

---
+++ Desktop/Fantasy Villages/Assets/Scripts/ControllCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ControllCamera : MonoBehaviour
+{
+    public float CameraSpeed;
+    public float ScrollSpeed;
+    private Vector3 MouseCameraController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+        ScrollCamera();
+    }
+
+    void MoveCamera()
+    {
+        if (Input.mousePosition.x >= Screen.width || Input.mousePosition.x <= 0)
+        {
+            transform.position = transform.position + new Vector3((Screen.width / 2 - Input.mousePosition.x) / Mathf.Abs((Screen.width / 2 - Input.mousePosition.x)) * Time.deltaTime * CameraSpeed * -1, 0, 0);
+        }
+        if (Input.mousePosition.y >= Screen.height || Input.mousePosition.y <= 0)
+        {
+            transform.position = transform.position + new Vector3(0, 0, (Screen.height / 2 - Input.mousePosition.y) / Mathf.Abs((Screen.height / 2 - Input.mousePosition.y)) * Time.deltaTime * CameraSpeed * -1);
+        }
+    }
+
+    void ScrollCamera()
+    {
+        transform.position = transform.position + new Vector3(0, Input.mouseScrollDelta.y * ScrollSpeed * Time.deltaTime * -1, 0);
+    }
+}
---
+++ Desktop/Fantasy Villages/Assets/Scripts/GameManager_Script.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class GameManager_Script : MonoBehaviour
+{
+    List <GameObject> ControlledCreatures = new List<GameObject>();
+
+    [SerializeField] private float speed;
+    private Vector3 movingPoint = new Vector3(0,0,0);
+    bool moveCommand = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MoveControlledCreatures", 0.0f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChooseControlledCreatures();
+        GiveMoveCommand();
+        MoveControlledCreatures();
+    }
+
+    void ChooseControlledCreatures ()
+    {
+        if (ControlledCreatures.Count != 0 && Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.A))
+        {
+            ControlledCreatures.Clear();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitEvent;
+            if (Physics.Raycast(camRay, out hitEvent))
+            {
+                if (hitEvent.collider.transform.tag == "Creature")
+                {
+                    ControlledCreatures.Add(hitEvent.collider.gameObject);
+                    //Debug.Log(hitEvent.collider.gameObject.name);
+                }
+            }
+        }
+    }
+
+    void GiveMoveCommand ()
+    {
+        if (Input.GetMouseButtonDown(1))
+
+        {
+            Ray camRay1 = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitEvent1;
+            if (Physics.Raycast(camRay1, out hitEvent1))
+            {
+                if (hitEvent1.collider.transform.tag == "Plane")
+                {
+                    movingPoint = new Vector3(hitEvent1.point.x, 23, hitEvent1.point.z);
+                    //moveCommand = true;
+                }
+            }
+        }
+    }
+
+    void MoveControlledCreatures()
+    {
+        for (int i = 0; i < ControlledCreatures.Count; i++)
+        {
+            ControlledCreatures[i].transform.position = Vector3.MoveTowards(ControlledCreatures[i].transform.position, movingPoint, speed * Time.deltaTime);
+        }
+    }
+}
