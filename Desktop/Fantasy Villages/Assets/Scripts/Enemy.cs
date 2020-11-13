c1b01d998f7425790fae88e969963c045bcc0aeb
cniki006 <cniki006@edu.xamk.fi>
2020-11-13 17:31:23 +0200

    Controll area, and enemy spawner

---
+++ Desktop/Fantasy Villages/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] public float Ehealth;
+    [SerializeField] private float speed;
+    private Vector3 movePoint;
+    public float HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Move", 0.0f, 0.01f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePoint = GameObject.Find("TownCenter").transform.position;
+        if (HP <= 0)
+        {
+            HP = 0;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Creature" || other.tag == "Building")
+        {
+            CreatureBehavior creatureStats = other.GetComponent<CreatureBehavior>();
+            PlayersBuilding buildingStats = other.GetComponent<PlayersBuilding>();
+            creatureStats.health -= 10;
+            buildingStats.BHealth -= 10;
+        }
+    }
+
+    void Move()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint, speed * Time.deltaTime);
+    }
+}
---
+++ Desktop/Fantasy Villages/Assets/Scripts/OrcSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrcSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemy1;
+    public int waveSize;
+    float waveTimer;
+    public float timerCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveTimer = timerCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerCount -= Time.deltaTime;
+        if (timerCount <= 0)
+        {
+            spawnWave(); 
+            waveSize += 2;
+            timerCount = waveTimer + 1;
+        }
+    }
+
+    void spawnWave()
+    {
+        for (int i = 0; i < waveSize; i++)
+        {
+            Instantiate(enemy1, new Vector3(transform.position.x + Random.Range(-100, 100), 0, transform.position.z + Random.Range(-100, 100)), Quaternion.identity);
+        }
+    }
+
+
+}
